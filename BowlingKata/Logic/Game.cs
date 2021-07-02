d0fb809abf3133417adaa173d6c59b1244be8c34
shraddha <shraddha.kapse@ao.com>
2021-07-02 17:30:09 +0100

    First Commit for Bowling Kata

---
+++ BowlingKata/Logic/Game.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+
+namespace BowlingKata.Logic
+{
+    public class Game
+    {
+        private readonly int[] _rolls = new int[21];
+        private int _currentRoll;
+        private int _totalScore;
+
+        public void Roll(int pins)
+        {
+            _rolls[_currentRoll] = pins;
+            _currentRoll++;
+        }
+        
+        public int Score()
+        {
+            _currentRoll = 0;
+
+            for (int frame = 1; frame < 11; frame++)
+            {
+                if (IsStrike())
+                {
+                    AddStrikeBonus();
+                }
+                else if (IsSpare())
+                {
+                    AddSpareBonus();
+                }
+                else
+                {
+                    AddNormalScore();
+                }
+            }
+
+            return _totalScore;
+        }
+
+        private void AddNormalScore()
+        {
+            _totalScore += _rolls[_currentRoll] + _rolls[_currentRoll + 1];
+            _currentRoll += 2;
+        }
+
+        private void AddSpareBonus()
+        {
+            _totalScore += 10 + _rolls[_currentRoll + 2];
+            _currentRoll += 2;
+        }
+        private void AddStrikeBonus()
+        {
+            _totalScore += 10 + _rolls[_currentRoll + 1] +_rolls[_currentRoll + 2];
+            _currentRoll += 1;
+        }
+        private bool IsSpare()
+        {
+            return _rolls[_currentRoll] + _rolls[_currentRoll + 1] == 10;
+        }
+        private bool IsStrike()
+        {
+            return _rolls[_currentRoll] == 10;
+        }
+    }
+}
---
+++ BowlingKata/Tests/Tests.cs
@@ -0,0 +1,113 @@
+﻿using BowlingKata.Logic;
+using NUnit.Framework;
+
+namespace BowlingKata.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private Game _game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _game = new Game();
+        }
+        
+        [Test]
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        [TestCase(0, 0)]
+        [TestCase(10, 10)]
+        public void Roll_SinglePin_ScoreIsOne(int pins, int expectedScore)
+        {
+            _game.Roll(pins);
+            var result = _game.Score();
+
+            Assert.AreEqual(expectedScore, result);
+        }
+
+        [Test]
+        public void Roll_MultiplePins_ScoreIsCorrect()
+        {
+            _game.Roll(4);
+            _game.Roll(4);
+
+            var result = _game.Score();
+            Assert.AreEqual(8, result);
+        }
+        
+         [Test]
+         public void Roll_SingleSpare_FirstFrame_ScoreIsCorrect()
+         {
+             _game.Roll(5);
+             _game.Roll(5);
+        
+             _game.Roll(1);
+             var result = _game.Score();
+             Assert.AreEqual(12, result);
+         }
+        
+        [Test]
+        public void Roll_TwoSpares_ScoreIsCorrect()
+        {
+            RollTwoSpares();
+
+            var result = _game.Score();
+            Assert.AreEqual(31, result);
+        }
+        
+        [Test]
+        public void Roll_SingleStrike_ScoreIsCorrect()
+        {
+            _game.Roll(10);
+            _game.Roll(2);
+            _game.Roll(2);
+            
+            var result = _game.Score();
+            Assert.AreEqual(18, result);
+        }
+
+        [Test]
+        public void Roll_MultipleStrikes_ScoreIsCorrect()
+        {
+            RollTwoStrikes();
+
+            var result = _game.Score();
+            Assert.AreEqual(40, result);
+        }
+
+        private void RollTwoStrikes()
+        {
+            _game.Roll(10);
+            _game.Roll(10);
+
+            _game.Roll(2);
+            _game.Roll(2);
+        }
+
+        [Test]
+        public void Roll_PerfectGame_ScoreIs300()
+        {
+            for (int roll = 0; roll < 12; roll++)
+            {
+                _game.Roll(10);
+            }
+            
+            var result = _game.Score();
+            Assert.AreEqual(300, result);
+        }
+
+        private void RollTwoSpares()
+        {
+            _game.Roll(3);
+            _game.Roll(7);
+
+            _game.Roll(5);
+            _game.Roll(5);
+
+            _game.Roll(2);
+            _game.Roll(2);
+        }
+    }
+}
